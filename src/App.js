7012b4a5dd32fb3dab80dd04bcb68080225ceccd
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-04-05 10:49:52 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+<div className='container'>
+      <header className="header">
+        <h1>< a href='/'>Benjamin Jack Pitts</a></h1>
+        <h2>Musician</h2>
       </header>
-    </div>
+      <Test />
+<div className='profile'><img src='https://i.imgur.com/A7lHurv.jpg' /></div>
+
+
+
+<footer>
+
+<h2>Built by: <a href='https://benjaminjackpitts.dev/'>Benjamin Jack Pitts</a>2021</h2></footer>
+</div>
   );
 }
 
---
+++ src/components/Test.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+function Test() {
+  return (
+<div className='container'>
+
+<h1>This is being rendered from the 'Test' component</h1>
+
+</div>
+  );
+}
+
+export default Test;
